e3e3ecb89a69639499671a77de687e5517479631
Sam <sam@Sams-MacBook-Air.local>
2021-03-05 21:26:48 -0500

    commit tree e48cc45c7191d3d3930f6c8145bac75049537598

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,84 @@
 class TweetsController < ApplicationController
+    get "/tweets" do
+        if !Helpers.is_logged_in?(session)
+            redirect to "/login"
+        else
+            @tweets = Tweet.all
+            @user = Helpers.current_user(session)
 
+            erb :"tweets/tweets"
+        end
+    end
 
+    get "/tweets/new" do
+        if !Helpers.is_logged_in?(session)
+            redirect to "/login"
+        end
+
+        erb :"tweets/new"
+    end
+
+    post "/tweets" do
+        user = Helpers.current_user(session)
+
+        if params[:content].empty?
+            redirect to "/tweets/new"
+        end
+        tweet = Tweet.create(:content => params[:content], :user_id => user.id)
+
+        redirect to "/tweets"       
+    end
+
+    get "/tweets/:id" do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find_by_id(params[:id])
+            
+            erb :"tweets/show"
+        else
+          redirect to "/login"
+        end
+    end
+
+    get "/tweets/:id/edit" do
+        if !Helpers.is_logged_in?(session)
+            redirect to "/login"
+        end
+
+        @tweet = Tweet.find_by_id(params[:id])
+
+        if Helpers.current_user(session).id != @tweet.user_id
+            redirect to "/tweets"
+        end
+
+        erb :"tweets/edit"
+    end
+
+    patch "/tweets/:id" do
+        tweet = Tweet.find_by_id(params[:id])
+
+        if params[:content].empty?
+            redirect to "/tweets/#{params[:id]}/edit"
+        end
+        tweet.update(:content => params[:content])
+
+        tweet.save
+
+        redirect to "/tweets/#{tweet.id}"
+    end
+
+    delete "/tweets/:id/delete" do
+        if !Helpers.is_logged_in?(session)
+            redirect to "/login"
+        end
+
+        @tweet = Tweet.find_by_id(params[:id])
+
+        if Helpers.current_user(session).id != @tweet.user_id
+            redirect to "/tweets"
+        end
+
+        @tweet.delete
+
+        redirect to "/tweets"
+    end
 end
